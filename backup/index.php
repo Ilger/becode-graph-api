4dfb0a557c33b1aae250aa4e0f0c9df8f3488fb3
Ilger <ilgerdepraetere@gmail.com>
2019-07-30 10:20:37 +0200

    fix htaccess file and rewrite .php. make return a json

---
+++ backup/index.php
@@ -0,0 +1,12 @@
+
+    <?php
+        header('Content-Type: application/json');
+
+$randomArray = [];
+for ($i = 0; $i < $_GET['length']; $i++){
+    $randomNumber = mt_rand(-20,20);
+    $genArray = [$i, $randomNumber];
+    array_push($randomArray, $genArray);
+}
+echo json_encode($randomArray);
+    ?>
